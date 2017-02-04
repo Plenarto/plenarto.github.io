eccd57561cf16e9237e0fcc89ea2ac622e8f4225
plenarto <p.k.lenartowicz@gmail.com>
2017-02-04 08:41:46 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,53 @@
+/* toggle menu on click */
+$(document).ready(function(){
+    $("#menu-icon").click(function(){
+        $("#main-nav").toggle();
+    });
+});
+/* menu shows on window resize */
+$(window).resize(function(){
+       if ($(window).width() > 1000) {
+            $("#main-nav").show();
+       }
+       else {
+            $("#main-nav").hide();
+       }
+});
+
+
+/* SMOOTH SCROLL */
+$(document).ready(function(){
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+/* BACK TO TOP BUTTON */
+
+jQuery(document).ready(function() {
+  var offset = 250;
+  var duration = 300;
+  jQuery(window).scroll(function() {
+    if (jQuery(this).scrollTop() > offset) {
+    jQuery(".back-to-top").fadeIn(duration);
+    } else {
+    jQuery(".back-to-top").fadeOut(duration);
+    }
+  });
+
+  jQuery(".back-to-top").click(function(event) {
+    event.preventDefault();
+    jQuery("html, body").animate({scrollTop: 0}, duration);
+    return false;
+  })
+});
