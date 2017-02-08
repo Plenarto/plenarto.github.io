908f9578819147028e8072a055e6309ed41f0218
plenarto <p.k.lenartowicz@gmail.com>
2017-02-08 21:49:39 +0100

    overflow cd

--- scripts.js
+++ scripts.js
@@ -51,3 +51,13 @@ jQuery(document).ready(function() {
     return false;
   })
 });
+
+/* IE smooth scroll bug fix */
+if(navigator.userAgent.match(/MSIE 10/i) || navigator.userAgent.match(/Trident\/7\./) || navigator.userAgent.match(/Edge\/12\./)) {
+    $('body').on("mousewheel", function () {
+      event.preventDefault();
+      var wd = event.wheelDelta;
+      var csp = window.pageYOffset;
+      window.scrollTo(0, csp - wd);
+    });
+  }
